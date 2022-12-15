5bbce6848378cf7f1c8d450de1a8275c1cee5b68
mmmarinasel <mmmarinasel@icloud.com>
2022-12-15 19:02:45 +0600

    Initial Commit

---
+++ mmmarinasel_weather_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mmmarinasel_weather_app
+//
+//  Created by Марина Селезнева on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
